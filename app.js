ac5836c5b6ca7019bdc7a41298ae64797839050d
Ayan <duttaayan070@gmail.com>
2025-08-16 23:38:01 +0530

    Review section added

--- app.js
+++ app.js
@@ -125,6 +125,14 @@ app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
   await listing.save();
   res.redirect(`/listings/${listing._id}`);
 }));
+//delete route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+  const {id,reviewId}=req.params;
+  await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+  await Review.findByIdAndDelete(reviewId);
+
+  res.redirect(`/listings/${id}`);
+}));
 
 // app.all("*",(req,res,next)=>{
 //   next(new ExpressError(404,"Page Not Found!"));
